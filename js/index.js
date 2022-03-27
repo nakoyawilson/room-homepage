498d1d38875da381e552931fd3a1057f22cdfeb3
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-03-27 11:58:09 -0400

    Add mobile menu functionality

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 const prevButton = document.getElementById("prev");
 const nextButton = document.getElementById("next");
 const slides = document.querySelectorAll(".slide");
+const open = document.getElementById("btn-open");
+const close = document.getElementById("btn-close");
+const nav = document.querySelector(".nav");
+const links = document.querySelectorAll(".nav-link");
 
 let activeSlide = 0;
 
@@ -23,4 +27,22 @@ prevButton.addEventListener("click", () => {
     activeSlide = slides.length - 1;
   }
   setActiveSlide();
+});
+
+open.addEventListener("click", () => {
+  nav.classList.add("open");
+  document.body.classList.add("lock-scroll");
+});
+
+close.addEventListener("click", () => {
+  nav.classList.remove("open");
+  document.body.classList.remove("lock-scroll");
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    nav.classList.remove("open");
+    document.body.classList.remove("lock-scroll");
+    e.preventDefault();
+  });
 });
